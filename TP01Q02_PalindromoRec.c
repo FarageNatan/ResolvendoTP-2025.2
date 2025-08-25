faa25c3b1e04a4395403bad58dc00346a7f2c1e6
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-08-25 10:13:23 -0300

    modificacoes sem a biblioteca string.h

--- TP01Q02_PalindromoRec.c
+++ TP01Q02_PalindromoRec.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 
-// Função recursiva para verificar se é palíndromo
 bool isPalindromoRec(char palavra[], int inicio, int final) {
     if (inicio >= final) {
         return true;
@@ -14,11 +12,21 @@ bool isPalindromoRec(char palavra[], int inicio, int final) {
     return isPalindromoRec(palavra, inicio + 1, final - 1);
 }
 
+int contarCaracteres(char palavra[]) {
+    int i = 0;
+    while (palavra[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
+
 int main() {
     char palavra[1000];
     scanf(" %[^\n]", palavra);
-    while (!(strlen(palavra) == 3 && palavra[0] == 'F' && palavra[1] == 'I' && palavra[2] == 'M')) {
-        bool palindromo = isPalindromoRec(palavra, 0, strlen(palavra) - 1); 
+    int tamanho = contarCaracteres(palavra);
+    while (!(palavra[0] == 'F' && palavra[1] == 'I' && palavra[2] == 'M' && palavra[3] == '\0')) {
+        bool palindromo = isPalindromoRec(palavra, 0, tamanho - 1);
         if (palindromo) {
             printf("SIM\n");
         } else {
