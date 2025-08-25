99ba630e46daabc350e4cfc4cef3fa8be6ac733d
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-08-25 11:11:55 -0300

    exercicio 3 + inclusao MyIO

---
+++ TP01Q03_CiframentoDeCesar.java
@@ -0,0 +1,20 @@
+public class TP01Q03_CiframentoDeCesar {
+
+    public static String ciframentoCesar(String palavra){
+        char[] palavraModificada = new char[palavra.length()];
+        for(int i = 0; i < palavra.length(); i++){
+            palavraModificada[i] = (char)(palavra.charAt(i)+3);
+        }
+        return new String(palavraModificada);
+    }
+
+    
+    public static void main(String[] args) {
+        String palavra = MyIO.readLine();
+        while(palavra.length() != 3 || palavra.charAt(0) != 'F' || palavra.charAt(1) != 'I' || palavra.charAt(2) != 'M'){
+            String palavraModificada = ciframentoCesar(palavra);
+            MyIO.println(palavraModificada);
+            palavra = MyIO.readLine();
+        }
+    }
+}
